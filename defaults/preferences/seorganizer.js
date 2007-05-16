c9d04b1c0af27f83b1e194352e2141d6cac020e4
maltekraus <maltekraus@b3bb5a03-f12e-0410-9e2c-0bfe072a7f21>
2007-05-16 20:58:13 +0000

    git-svn-id: https://organize-search-engines.googlecode.com/svn/trunk@12 b3bb5a03-f12e-0410-9e2c-0bfe072a7f21

--- defaults/preferences/seorganizer.js
+++ defaults/preferences/seorganizer.js
@@ -1,4 +1,10 @@
 pref("extensions.seorganizer.sortDirection", "natural");
 
+pref("extensions.seorganizer.reporter.sendReports", true);
+pref("extensions.seorganizer.reporter.confirmSending", true);
+
+pref("extensions.seorganizer.reporter.url",
+     "http://maltekraus.de/Firefox/search-tools/reporter.php?ext=%ITEM_ID%&extversion=%ITEM_VERSION%&hostapp=%APP_ID%&hostversion=%REQ_VERSION%&os=%APP_OS%&reqversion=%REQ_VERSION%");
+
 pref("extensions.organize-search-engines@maltekraus.de.description",
      "chrome://seorganizer/locale/desc.properties");
