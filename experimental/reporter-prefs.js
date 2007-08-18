8b41bbd72ffb8bf409105222826c754c3c9bb5a0
maltekraus <maltekraus@b3bb5a03-f12e-0410-9e2c-0bfe072a7f21>
2007-08-18 15:14:29 +0000

    move reporter prefs to experimental
    git-svn-id: https://organize-search-engines.googlecode.com/svn/trunk@49 b3bb5a03-f12e-0410-9e2c-0bfe072a7f21

---
+++ experimental/reporter-prefs.js
@@ -0,0 +1,5 @@
+pref("extensions.seorganizer.reporter.sendReports", true);
+pref("extensions.seorganizer.reporter.confirmSending", true);
+
+pref("extensions.seorganizer.reporter.url",
+     "http://maltekraus.de/Firefox/search-tools/reporter.php?ext=%ITEM_ID%&extversion=%ITEM_VERSION%&hostapp=%APP_ID%&hostversion=%REQ_VERSION%&os=%APP_OS%");
