383f7162962ca5010b53ce506b39ed3e3453a4cb
maltekraus <maltekraus@b3bb5a03-f12e-0410-9e2c-0bfe072a7f21>
2007-11-19 19:40:06 +0000

    some reworking license header
    git-svn-id: https://organize-search-engines.googlecode.com/svn/branches/1.1@89 b3bb5a03-f12e-0410-9e2c-0bfe072a7f21

--- chrome/content/engineManager.js
+++ chrome/content/engineManager.js
@@ -1,4 +1,38 @@
-try {
+/* ***** BEGIN LICENSE BLOCK *****
+Version: MPL 1.1/GPL 2.0/LGPL 2.1
+
+The contents of this file are subject to the Mozilla Public License Version
+1.1 (the "License"); you may not use this file except in compliance with
+the License. You may obtain a copy of the License at
+http://www.mozilla.org/MPL/
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+for the specific language governing rights and limitations under the
+License.
+
+The Original Code is Organize Search Engines.
+
+The Initial Developer of the Original Code is
+Malte Kraus.
+Portions created by the Initial Developer are Copyright (C) 2006-2007
+the Initial Developer. All Rights Reserved.
+
+Contributor(s):
+  Malte Kraus <mails@maltekraus.de> (Original author)
+
+ Alternatively, the contents of this file may be used under the terms of
+ either the GNU General Public License Version 2 or later (the "GPL"), or
+ the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ in which case the provisions of the GPL or the LGPL are applicable instead
+ of those above. If you wish to allow use of your version of this file only
+ under the terms of either the GPL or the LGPL, and not to allow others to
+ use your version of this file under the terms of the MPL, indicate your
+ decision by deleting the provisions above and replace them with the notice
+ and other provisions required by the GPL or the LGPL. If you do not delete
+ the provisions above, a recipient may use your version of this file under
+ the terms of any one of the MPL, the GPL or the LGPL.
+***** END LICENSE BLOCK ***** */
 
 const Ci = Components.interfaces, Cc = Components.classes;
 
@@ -164,10 +198,10 @@ EngineManagerDialog.prototype = {
   observe: function EngineManager__observe(aSubject, aTopic, aVerb) {
     window.setTimeout(function() {
       if(aTopic === "browser-search-engine-modified" &&
-         aEngine instanceof Ci.nsISearchEngine) {
+         aSubject instanceof Ci.nsISearchEngine) {
         switch (aVerb) {
           case "engine-added":
-            gEngineView.addEngine(aEngine);
+            gEngineView.addEngine(aSubject);
             gEngineView.rowCountChanged(gEngineView.lastIndex, 1);
             break;
           case "engine-changed":
@@ -424,7 +458,7 @@ EngineManagerDialog.prototype = {
           engine.hidden = false;
         node = gSEOrganizer.getItemByName(engine.name);
         var idx = gRemovedEngines.indexOf(node);
-        if(node && idx !== -1) {
+        if(node && idx != -1) {
           gRemovedEngines = gRemovedEngines.slice(0, idx)
                                         .concat(gRemovedEngines.slice(idx + 1));
         }
@@ -432,7 +466,8 @@ EngineManagerDialog.prototype = {
         if(!node) {
           node = gSEOrganizer._getAnonymousResource();
         }
-        item = new Structure__Item(gEngineView._structure, node, engine);
+        item = new Structure__Item(parent, node, engine);
+        item.modified = true;
         this.showRestoreDefaults(defaults.length !== 1);
         break;
     }
@@ -663,7 +698,7 @@ function Structure__Item(parent, node, engine) {
   this.parent = parent;
   this.node = node;
   this.modified = false;
-  if(!(engine instanceof Ci.nsISearchEngine))
+  if(!engine || !(engine instanceof Ci.nsISearchEngine))
     engine = null;
 
   var rdfService = Cc["@mozilla.org/rdf/rdf-service;1"]
@@ -725,7 +760,7 @@ Structure__Item.prototype = {
         os.notifyObservers(this.originalEngine, SEARCH_ENGINE_TOPIC, "engine-changed");
         os.addObserver(gSEOrganizer, SEARCH_ENGINE_TOPIC, false);
 
-        /*var rdfService = Cc["@mozilla.org/rdf/rdf-service;1"]
+        var rdfService = Cc["@mozilla.org/rdf/rdf-service;1"]
                            .getService(Ci.nsIRDFService);
         var namePred = rdfService.GetResource(NS + "Name");
         oldName = rdfService.GetLiteral(oldName);
@@ -733,7 +768,7 @@ Structure__Item.prototype = {
                             rdfService.GetLiteral(this.name), true);
         try {
           gSEOrganizer.Unassert(this.node, namePred, oldName);
-        } catch(e) {}*/
+        } catch(e) {}
       }
       if(gSEOrganizer.getNameByItem(this.node) != this.name) {
         var rdfService = Cc["@mozilla.org/rdf/rdf-service;1"]
@@ -1259,9 +1294,6 @@ EngineView.prototype = {
     return open;
   }
 };
-} catch(e) {
-  Components.reportError(e);
-}
 function LOG(msg) {
   msg = "Organize Search Engines:   " + msg;
   var consoleService = Cc["@mozilla.org/consoleservice;1"]
