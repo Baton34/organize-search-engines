e0fd7b23b74f2f354944caf575b200447e718030
maltekraus <maltekraus@b3bb5a03-f12e-0410-9e2c-0bfe072a7f21>
2008-05-16 16:26:57 +0000

    be compatible to the jEdit javascript plugin
    git-svn-id: https://organize-search-engines.googlecode.com/svn/trunk@232 b3bb5a03-f12e-0410-9e2c-0bfe072a7f21

--- chrome/content/searchServiceModifications.js
+++ chrome/content/searchServiceModifications.js
@@ -1,4 +1,4 @@
-/* -*- js-standard: mozscript; js-import:; js-var:SearchService; -*- */
+/* -*- js-standard: mozscript; js-var:SearchService,Engine,notifyAction; -*- */
 /* ***** BEGIN LICENSE BLOCK *****
 Version: MPL 1.1/GPL 2.0/LGPL 2.1
 
