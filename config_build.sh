f0f6609e5fbbc5a17bd6f16d1325ade0395cfbd9
maltekraus <maltekraus@b3bb5a03-f12e-0410-9e2c-0bfe072a7f21>
2009-02-08 16:58:18 +0000

    new way of creating xpi's
    git-svn-id: https://organize-search-engines.googlecode.com/svn/trunk@247 b3bb5a03-f12e-0410-9e2c-0bfe072a7f21

---
+++ config_build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Build config for build.sh
+APP_NAME=organize-search-engines
+CHROME_PROVIDERS="content skin locale"
+CLEAN_UP=1
+ROOT_FILES=
+ROOT_DIRS="defaults components"
+BEFORE_BUILD="../components.sh $APP_NAME nsISEOrganizer"
+AFTER_BUILD=
+VERSION="1.5pre"
